684e5c176eaaf395ad656560d9ad34685b139054
Zachary-Maizes <zmaizes@gmail.com>
2023-04-09 16:40:15 -0400

    A9 - 4.1-4.3

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ import UserController
   from "./controllers/users/users-controller.js"
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 const app = express()
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+mongoose.connect(CONNECTION_STRING);
 TuitsController(app);
 HelloController(app)
 UserController(app)
